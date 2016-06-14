896b13f640654187d6a83c78d361ceb7111299ec
Jack Cooper <JCooper@GRAPEVINE.local>
2016-06-14 16:37:57 +0100

    Adding all files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Jack Cooper | Web Developer</title>
+    <link rel="stylesheet" href="assets/styles/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/styles/styles.css">
+</head>
+<body>
+ 
+     <div class="container-fluid">
+         <div class="background">
+             
+         </div>
+     </div>
+
+  
+
+    
+    <script src="assets/js/jquery-1.12.3.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
+
+
