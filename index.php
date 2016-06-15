4472c1c9b37b10711f0b6251857b09b7ca2c59dd
Jack Cooper <JCooper@GRAPEVINE.local>
2016-06-15 17:21:01 +0100

    Add files and updated index

--- index.php
+++ index.php
@@ -3,27 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Jack Cooper | Web Developer</title>
+     <link rel="stylesheet" href="assets/styles/animate.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="assets/styles/bootstrap.min.css">
     <link rel="stylesheet" href="assets/styles/styles.css">
 </head>
 <body>
- 
+
         <div class="container-fluid">
          <div class="welcome">
-            
-            <h1>Welcome to Code Coops</h1>
-            <h4>Site under construction, but don't get too excited</h4>
-             
+
+            <h1>Welcome to Code Coop The website</h1>
+            <p class="animate hinge">Site under construction, but don't get too excited</p>
          </div>
     </div>
-     
-     
 
-    
+
+
+
     <script src="assets/js/jquery-1.12.3.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/script.js"></script>
 </body>
 </html>
-
-
